04360fa949ca3fe0d6e6ed5a14bf5c13c9666044
WhiteAsparagus1101 <55292680+WhiteAsparagus1101@users.noreply.github.com>
2021-03-01 18:51:37 +0900

    Update func.js

--- func.js
+++ func.js
@@ -1,4 +1,4 @@
 function coreCheck() {
     console.log(window.navigator.hardwareConcurrency);
-    document.getElementById("numberOfCore").innerHTML = "あなたのPCのコアの数は" + window.navigator.hardwareConcurrency + "です";
+    document.getElementById("numberOfCore").innerHTML = "あなたのPCのコアの数は" + window.navigator.hardwareConcurrency + "です。（スレッド数かも）";
 }
